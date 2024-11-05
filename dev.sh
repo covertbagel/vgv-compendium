9e7de313ced14cd974ac6eda3ddc91d62feacb8b
covertbagel <vgv-compendium@covertbagel.com>
2024-11-05 00:52:39 -0800

    Fix `dev.sh` for new versions of Cloud SDK and Python3

--- dev.sh
+++ dev.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # This needs to be run from w/i this directory.
+# Do `gcloud auth application-default login` to fix "invalid_grant" errors
 
-# GOOGLE_CLOUD_PROJECT must be changed to match your own project name!
-# TODO: Configure GOOGLE_CLOUD_PROJECT outside of git?
+# `--application=` must be changed to match your own project name!
+# TODO: Configure `--application=` outside of git?
 
 # This ends up using the "production" datastore, so need to be careful
 # not to mess things up too much!
 # TODO: Figure out how to have a separate "dev" datastore?
 
-GOOGLE_CLOUD_PROJECT=vgv-compendium dev_appserver.py app.yaml
+dev_appserver.py --application=vgv-compendium --runtime_python_path=/usr/bin/python3 app.yaml
 
